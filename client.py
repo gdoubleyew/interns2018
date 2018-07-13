eefa747cbda6981622465e1f91069f62206bcf50
F Wallace <fwallace@waltz.pni.princeton.edu>
2018-07-13 13:26:21 -0400

    updated to handle curent test functions (version 1.6)(maybe)

--- client.py
+++ client.py
@@ -2,11 +2,12 @@ import requests
 import pickle
 import argparse
 import time
-import random
+import os
 # import subprocess
 
-#client to run analasis
-class AClient(object):
+
+# client to run analasis
+class Client(object):
     """A class to communicate with a REST server.
     """
 
@@ -28,121 +29,138 @@ class AClient(object):
             self.gateway_address,
             self.gateway_port)
 
-    def put(self, request_id, value):
+    def put(self, url_P2, request_id, value):
         data = pickle.dumps(value)
-        res = requests.post(url=self.url + '/' + "analysis",
+        res = requests.post(url=self.url + '/' + url_P2,
                             files={
                                 "value": data,
                                 "request_id": request_id
                             })
         return res
 
-    def get(self, params):
-        res = requests.get(url=self.url + '/' + "analysis", params=params)
+    def get(self, url_P2, params):
+        res = requests.get(url=self.url + '/' + url_P2, params=params)
         # objects = self.serialization_context.deserialize(res.raw.read())
         return res
 
-#client to calculate Delta
-class DClient(object):
-    """A class to communicate with a REST server.
-    """
 
-    def __init__(self,
-                 gateway_address,
-                 gateway_port=5200):
-        """Initialize a new client.
+def calcClockSkew(client, file=None, reset=True, test_offset=0):
+    clockSkew = None
 
-        Args:
-            gateway_address (str): The IP address of the head node / gateway.
-            gateway_port (int): The gateway's port.
+    if client is None:
+        print("No client spesified")
+        quit()
+    # if no file is entered (make default file name)
+    if not isinstance(file, str):
+        file = "Default.txt"
+    # reset defaulting to true
+    if not isinstance(reset, bool):
+        reset = True
+    # opens file with perditermined clock diffrence
+    if not reset:
+        if os.path.exists(file):
+            with open(file, "r") as Dfile:
+                clockSkew = Dfile.read()
+    # test_offset defaults to 0
+    if not isinstance(test_offset, float) and not isinstance(test_offset, int):
+        test_offset = 0
+    # recalculate ClockSkew if needed
+    if not clockSkew:
+        RTT_list = []
+        clockSkew_list = []
+        for i in range(100):
+            client_ts = time.time()
+            params = {
+                "request_id": 1,
+                "client_ts": client_ts,
+                "test_offset": test_offset
+            }
+            res = client.get("calcClockSkew", params)
+            vals = res.json()
+            client_ts2 = time.time()
+            server_ts = vals['server_ts']
 
-        Returns:
-            A new Client object
-        """
-        self.gateway_address = gateway_address
-        self.gateway_port = gateway_port
-        self.url = "http://{}:{}".format(
-            self.gateway_address,
-            self.gateway_port)
+            a = server_ts - client_ts       # I think this fomula is cousing an error
+            b = server_ts - client_ts2      #
+            RTT = a - b
+            clockSkew = (a + b)/2
 
-    def put(self, request_id, value):
-        data = pickle.dumps(value)
-        res = requests.post(url=self.url + '/' + "calcDelta",
-                            files={
-                                "value": data,
-                                "request_id": request_id
-                            })
-        return res
+            RTT_list.append(RTT)
+            clockSkew_list.append(clockSkew)
+        RTT = min(RTT_list)
+        clockSkew = clockSkew_list[RTT_list.index(RTT)]
 
-    def get(self, params):
-        res = requests.get(url=self.url + '/' + "calcDelta", params=params)
-        # objects = self.serialization_context.deserialize(res.raw.read())
-        return res
+        if file is not None:
+            with open(file, "w+") as Dfile:
+                Dfile.write("{}".format(clockSkew))
+        # deleting default file
+        if os.path.exists("Default.txt"):
+            os.remove("Default.txt")
+
+    return clockSkew
+
+
+def analysis(client, clockSkew_file=None, work_diff=0):
+    clockSkew = None
+
+    if client is None:
+        print("No client spesified")
+        quit()
+    # if no file is endered (it creat a temperary file)
+    if clockSkew_file is None:
+        with open("Default.txt", "w+") as Dfile:
+            Dfile.close()
+        with open("Default.txt", "r") as Dfile:
+            clockSkew = Dfile.read()
+            Dfile.close()
+            print("Please enter a file next time round.")
+    # cheaking work difficulty is usable
+    if isinstance(work_diff, float) or isinstance(work_diff, int):
+        # to make sure there are no negatives
+        work_diff = abs(work_diff)
+    else:
+        work_diff = 0
+
+        # check if ClockSkew is in file
+    if not clockSkew:
+        clockSkew = calcClockSkew(client, clockSkew_file, True)
+    # returnVals = [] #for testing
+    # succsesful = [] #for testing
+    # deadline_list = []
+    client_ts = time.time()
+    deadline = client_ts + 2 - float(clockSkew)
+    params = {
+        "request_id": 1,
+        "client_ts": client_ts,
+        "deadline": deadline,
+        "work_diff": work_diff
+    }
+    res = client.get("analysis", params)
+    vals = res.json()
+    final_ts = time.time()  # for testing
+    print(vals['return'])
+    time_took = final_ts - client_ts  # for testing
+    # succsesful.append(vals['return']) #for testing
+    # returnVals.append(time_took) #for testing
+    # deadline_list.append(deadline) #for testing
+    return (vals['return'], time_took, vals['deadline'])  # for testing
+    if os.path.exists("Default.txt"):
+        os.remove("Default.txt")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Realtime Client")
     parser.add_argument('-a', action="store", dest="addr", default="localhost")
     parser.add_argument('-p', action="store", dest="port", default=5200)
-    #parser.add_argument('-r', action="store", default = False)
+    # parser.add_argument('-r', action="store", default = False)
     args = parser.parse_args()
 
-    #opens file were Delta is (or is not)
-    Dfile = open("Delta_file.txt", "r+")
-    Delta = Dfile.read()
-    print(Delta)
-    #checks to see if delta is in file and skips this block if it is
-    if not Delta:
-        DClient = DClient(args.addr, args.port)
-        rtt_list = []
-        total = []
-        for i in range(0,100):
-            client_ts = time.time()
-            params = {
-            "request_id": 1,
-            "client_ts": client_ts
-            }
-            res = DCient.get(params)
-            client_ts2 = time.time()
-            vals = res.json()
-            server_ts = vals['server_ts']
+    # creats client object
+    client = Client(args.addr, args.port)
 
-            rtt = client_ts2 - client_ts
-            rtt_list.append(rtt)
+    calcClockSkew(client, "clockSkew_file.txt", True)
+    analysis(client, "clockSkew_file.txt", 2)
 
-            TT = rtt/2
-            D = client_ts2 - server_ts - TT
-            total.append(D)
-        #calculates delta and writes it to Dfile
-        min_T = min(rtt_list)
-        best_val = total[rtt_list.index(min_T)]
-        print("Total: {}".format(best_val))
-        Dfile.close()
-        Dfile = open("Delta_file.txt", "w+")
-        Dfile.write("{}".format(best_val))
-
-    Client = AClient(args.addr, args.port)
-
-    client_ts = time.time()
-    work_diff = random.randint(1, 3)
-    dedline = client_ts + 2 + float(Delta)
-    params = {
-    "request_id": 1,
-    "client_ts": client_ts,
-    "dedline": dedline,
-    "work_diff": work_diff
-    }
-    res = Client.get(params)
-    vals = res.json()
-    print(vals['return'])
 
 if __name__ == "__main__":
     main()
-
-
-# data = obj.to_buffer().to_pybytes()
-# res = requests.post(url="http://localhost:5000",
-#                     files={
-#                         "data": data,
-#                         "meta": pickle.dumps(123)
-#                     })
